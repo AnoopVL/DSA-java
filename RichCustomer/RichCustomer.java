c9f3954a770d62b1c9e0330e0b313dcc0428d4e9
AnoopVL <avlanjekar4@gmail.com>
2023-08-30 00:36:25 +0530

    richestCustomer started

---
+++ RichCustomer/RichCustomer.java
@@ -0,0 +1,11 @@
+//1672. Richest Customer Wealth
+//https://leetcode.com/problems/richest-customer-wealth/
+
+public class RichCustomer{
+    public static void main(String[] args) {
+        
+    }
+    public static int maximumWealth(int[][] accounts) {
+        return ans;
+    }
+}
