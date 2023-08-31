7f06f6973bb40ccae6c912cb767fe350f6314e74
AnoopVL <avlanjekar4@gmail.com>
2023-08-31 18:13:43 +0530

    problem solved

--- RichCustomer/RichCustomer.java
+++ RichCustomer/RichCustomer.java
@@ -6,6 +6,16 @@ public class RichCustomer{
         
     }
     public static int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for (int[] account2 : accounts) {
+            int sum = 0;
+            for (int element : account2) {
+                sum+=element;
+            }
+            if(sum > ans){
+                ans = sum;
+            }
+        }
         return ans;
     }
 }
