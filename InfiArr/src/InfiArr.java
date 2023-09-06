dd5206767d36cdfa4e7910792c532010f11a3562
AnoopVL <avlanjekar4@gmail.com>
2023-09-06 19:38:38 +0530

    bsearch funtion added

--- InfiArr/src/InfiArr.java
+++ InfiArr/src/InfiArr.java
@@ -2,4 +2,21 @@ public class InfiArr{
     public static void main(String[] args) {
         
     }
+    static int bs(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <=end){
+            int mid = (start + end) / 2;
+        if (arr[mid] == target){
+            return mid;
+        }
+        else if (target>arr[mid]){
+            start = mid +1;
+        }
+        else if (target<arr[mid]){
+            end = mid -1;
+        }
+    }
+    return -1;
+}
 }
