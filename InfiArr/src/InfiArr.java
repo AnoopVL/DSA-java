e8e47865f54812e71f4d3e1671c62031516c09d0
AnoopVL <avlanjekar4@gmail.com>
2023-09-08 23:37:19 +0530

    findingRange function added

--- InfiArr/src/InfiArr.java
+++ InfiArr/src/InfiArr.java
@@ -4,19 +4,21 @@ public class InfiArr{
         int target = 4;
         int ans = bs(arr, target);
     }
-    static int bs(int[] arr, int target){
+    static int ans(int[] arr,int target){
         int start = 0;
-        int end = arr.length -1;
+        int end = 0;
+    }
+    static int bs(int[] arr, int target, int start, int end){
         while(start <=end){
             int mid = (start + end) / 2;
-        if (arr[mid] == target){
-            return mid;
-        }
-        else if (target>arr[mid]){
+        if (target>arr[mid]){
             start = mid +1;
         }
         else if (target<arr[mid]){
             end = mid -1;
+        }
+        else{
+            return mid;
         }
     }
     return -1;
