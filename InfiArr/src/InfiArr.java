e686ed7a20a6c3a1292dd10caaa4b76971d3f40f
AnoopVL <avlanjekar4@gmail.com>
2023-09-11 00:29:15 +0530

    problem solved

--- InfiArr/src/InfiArr.java
+++ InfiArr/src/InfiArr.java
@@ -1,16 +1,18 @@
 public class InfiArr{
     public static void main(String[] args) {
-        int[] arr= {1,2,3,4,5,6,7,8,9,10};
-        int target = 4;
-        int ans = bs(arr, target);
+        int[] arr= {1,2,3,4,5,6,7,8,9,10,32,45,65,87,98,99,100,112,130,150,203,290,360,370,670,780,1200};
+        int target = 32;
+        System.out.println(ans(arr, target));
     }
     static int ans(int[] arr,int target){
         int start = 0;
         int end = 1;
         while(target>arr[end]){
             int newStart=end+1;
-            end=end+(end+start-1)*2;
+            end=end+(end-start+1)*2;
+            start = newStart;
         }
+        return bs(arr, target, start, end );
 
     }
     static int bs(int[] arr, int target, int start, int end){
