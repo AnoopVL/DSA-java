129254289cef3c790015f8b6299f79a982d146ac
AnoopVL <avlanjekar4@gmail.com>
2023-09-18 13:18:19 +0530

    Problem solved

--- BSearch/src/Bsearch.java
+++ BSearch/src/Bsearch.java
@@ -5,11 +5,11 @@ public class Bsearch{
         int ans = bs(arr, target);
         System.out.println(ans);
     }
-    static int bs(int[] arr, int target){
-        int start = 0;
-        int end = arr.length -1;
+    static int bs(int[] arr, int target, int start, int end){
+        // int start = 0;
+        // int end = arr.length -1;
         while(start <=end){
-            int mid = (start + end) / 2;
+            int mid =start+(end-start) / 2;
         if (arr[mid] == target){
             return mid;
         }
@@ -20,6 +20,6 @@ public class Bsearch{
             end = mid -1;
         }
     }
-    return -1;
-}
+        return -1;
+    }
 }
--- RotArr/src/RotArr.java
+++ RotArr/src/RotArr.java
@@ -1,8 +1,42 @@
-// package RotArr.src;
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+// 33. Search in Rotated Array
+
 public class RotArr{
     public static void main(String[] args) {
-        int[]arr={4,5,6,7,0,1,2};
-        System.out.println(pivot(arr));
+        int[]nums={4,5,6,7,0,1,2};
+        int target=1;
+        System.out.println(pivot(nums));
+        System.out.println(search(nums, target));
+    }
+    static int search(int[] nums, int target){
+        int pivot=pivot(nums);
+        if(pivot==-1){
+            return bs(nums, target, 0, nums.length-1);
+        }
+        if(pivot==target){
+            return pivot;
+        }
+        if(target>nums[0]){
+            return bs(nums, target, 0, pivot-1);
+        }
+        return bs(nums, target, pivot+1, nums.length-1);
+    }
+    static int bs(int[] arr, int target, int start, int end){
+        // int start = 0;
+        // int end = arr.length -1;
+        while(start <=end){
+            int mid =start+(end-start) / 2;
+        if (arr[mid] == target){
+            return mid;
+        }
+        else if (target>arr[mid]){
+            start = mid +1;
+        }
+        else if (target<arr[mid]){
+            end = mid -1;
+        }
+    }
+        return -1;
     }
     static int pivot(int[]arr){
         int start=0;
