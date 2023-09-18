13b3a1ebe2f6a0b821f1621e4f78dc94f9fcc519
AnoopVL <avlanjekar4@gmail.com>
2023-09-18 17:36:49 +0530

    error removed

--- RotArr/src/RotArr.java
+++ RotArr/src/RotArr.java
@@ -4,7 +4,7 @@
 public class RotArr{
     public static void main(String[] args) {
         int[]nums={4,5,6,7,0,1,2};
-        int target=1;
+        int target=3;
         System.out.println(pivot(nums));
         System.out.println(search(nums, target));
     }
@@ -13,7 +13,7 @@ public class RotArr{
         if(pivot==-1){
             return bs(nums, target, 0, nums.length-1);
         }
-        if(pivot==target){
+        if(nums[pivot]==target){
             return pivot;
         }
         if(target>nums[0]){
@@ -22,20 +22,18 @@ public class RotArr{
         return bs(nums, target, pivot+1, nums.length-1);
     }
     static int bs(int[] arr, int target, int start, int end){
-        // int start = 0;
-        // int end = arr.length -1;
         while(start <=end){
             int mid =start+(end-start) / 2;
-        if (arr[mid] == target){
-            return mid;
-        }
-        else if (target>arr[mid]){
+        if (target>arr[mid]){
             start = mid +1;
         }
         else if (target<arr[mid]){
             end = mid -1;
         }
-    }
+        else{
+            return mid;
+        }
+        }
         return -1;
     }
     static int pivot(int[]arr){
