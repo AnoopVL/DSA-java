c706d567a8d82a18b0714d3ba66e25cb69ea9eab
AnoopVL <avlanjekar4@gmail.com>
2024-01-05 23:57:07 +0530

    string builder

---
+++ stringBuilder/src/stringBuilder.java
@@ -0,0 +1,10 @@
+package stringBuilder.src;
+
+public class stringBuilder {
+    public static void main(String[] args) {
+        int[] a = {21,23,45,656,45};
+        int b = 68;
+        String name = "Anoop Lanjekar";
+        System.out.println(name);
+    }
+}
--- stringBuilder/stringBuilder.java
+++
@@ -1,9 +0,0 @@
-package stringBuilder;
-
-public class stringBuilder {
-    public static void main(String[] args) {
-        int i=0;
-        // another cheat commit :(
-        int b=231;
-    }
-}
