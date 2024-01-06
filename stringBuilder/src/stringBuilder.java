75f448efb441d1a312a440661dca7dad0b4877a4
AnoopVL <avlanjekar4@gmail.com>
2024-01-06 23:56:19 +0530

    string builder

--- stringBuilder/src/stringBuilder.java
+++ stringBuilder/src/stringBuilder.java
@@ -2,9 +2,13 @@ package stringBuilder.src;
 
 public class stringBuilder {
     public static void main(String[] args) {
-        int[] a = {21,23,45,656,45};
-        int b = 68;
+        int[] arr = {21,23,45,656,45};
+        int num = 68;
         String name = "Anoop Lanjekar";
         System.out.println(name);
+        String a = "anoop";
+        System.out.println(a);
+        a = "avl";
+        System.out.println(a);
     }
 }
