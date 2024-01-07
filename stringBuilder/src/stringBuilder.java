0248a2482b772471a650a0d124ea844ef3eb096f
AnoopVL <avlanjekar4@gmail.com>
2024-01-07 23:56:50 +0530

    string builder

--- stringBuilder/src/stringBuilder.java
+++ stringBuilder/src/stringBuilder.java
@@ -1,14 +1,18 @@
-package stringBuilder.src;
-
 public class stringBuilder {
     public static void main(String[] args) {
         int[] arr = {21,23,45,656,45};
         int num = 68;
         String name = "Anoop Lanjekar";
-        System.out.println(name);
+        System.out.println(name); //Anoop Lanjekar
         String a = "anoop";
-        System.out.println(a);
+        System.out.println(a); //anoop
         a = "avl";
-        System.out.println(a);
+        System.out.println(a); //avl
+        String a1 = "anoop";
+        String a2 = "anoop";
+        System.out.println(a1==a2); //true
+        String a3 = new String("anoop");
+        String a4 = new String("anoop");
+        System.out.println(a3==a4); //false
     }
 }
