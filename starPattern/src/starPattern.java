2db5d3c3a28800b106a33c5bc9b5f971f0ab926b
AnoopVL <avlanjekar4@gmail.com>
2024-01-16 00:12:01 +0530

    starPattern

---
+++ starPattern/src/starPattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class starPattern {
+    public static void main(String[] args) {
+        int row = 4;
+        int col = 4;
+        strPtrn(row, col);
+    }
+    public static void strPtrn(int row, int col){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <col; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
