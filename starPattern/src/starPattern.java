fe5ada4519cfe5965ed05c02620c0ba7ec503628
AnoopVL <avlanjekar4@gmail.com>
2024-01-17 23:14:19 +0530

    inverted left star pyramid

--- starPattern/src/starPattern.java
+++ starPattern/src/starPattern.java
@@ -6,14 +6,25 @@ public class starPattern {
         int col = 4;
         strPtrn(row, col);
     }
+    //inverted left star pyramid
     public static void strPtrn(int row, int col){
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j <=i; j++) {
+        for (int i = row; i >=0; i--) {
+            for (int j = i; j >=0; j--) {
                 System.out.print("*");
+                System.out.print("\t");
             }
             System.out.println("\n");
         }
     }
+    // left pyramid
+    // public static void strPtrn(int row, int col){
+    //     for (int i = 0; i < row; i++) {
+    //         for (int j = 0; j <=i; j++) {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println("\n");
+    //     }
+    // }
     // 4x4 star pattern
     // public static void strPtrn(int row, int col){
     //     for (int i = 0; i < row; i++) {
