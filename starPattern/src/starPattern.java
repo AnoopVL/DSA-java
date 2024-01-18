92617bbd79cc2add204ba23f0165b0342566f17a
AnoopVL <avlanjekar4@gmail.com>
2024-01-18 23:38:32 +0530

    right shift pyramid

--- starPattern/src/starPattern.java
+++ starPattern/src/starPattern.java
@@ -6,16 +6,32 @@ public class starPattern {
         int col = 4;
         strPtrn(row, col);
     }
-    //inverted left star pyramid
     public static void strPtrn(int row, int col){
-        for (int i = row; i >=0; i--) {
-            for (int j = i; j >=0; j--) {
+            for (int i = row; i >0 ; i--) {
+                // inner loop to print spaces.
+            for (int j = 1; j < i; j++) {
+                System.out.print("\t");
+                // System.out.print("\t");
+            }
+ 
+            // inner loop to print stars.
+            for (int j = 0; j <= row - i; j++) {
                 System.out.print("*");
                 System.out.print("\t");
             }
-            System.out.println("\n");
+                System.out.println("\n");
+            }
         }
-    }
+    //inverted left star pyramid
+    // public static void strPtrn(int row, int col){
+    //     for (int i = row; i >=0; i--) {
+    //         for (int j = i; j >=0; j--) {
+    //             System.out.print("*");
+    //             System.out.print("\t");
+    //         }
+    //         System.out.println("\n");
+    //     }
+    // }
     // left pyramid
     // public static void strPtrn(int row, int col){
     //     for (int i = 0; i < row; i++) {
