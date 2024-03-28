c5118b5a3a03db3f258eae88ca199e13b360484e
AnoopVL <avlanjekar4@gmail.com>
2024-03-28 22:23:12 +0530

    LeftTriangle left shift right triangle

--- RecursionPattern/src/RecursionPattern.java
+++ RecursionPattern/src/RecursionPattern.java
@@ -1,7 +1,8 @@
 public class RecursionPattern {
 
     public static void main(String[] args) {
-        reverseLeftTriangle(5, 0);
+        // reverseLeftTriangle(5, 0);
+        leftTriangle(5, 0);
     }
     public static void reverseLeftTriangle(int row, int col){
         if(row == 0){
@@ -14,6 +15,19 @@ public class RecursionPattern {
         else{
             System.out.println();
             reverseLeftTriangle(row-1, 0);
+        }
+    }
+    public static void leftTriangle(int row, int col){
+        if(row == 0){
+            return;
+        }
+        if(col<row){
+            leftTriangle(row, col+1);
+            System.out.print("* \t");
+        }
+        else{
+            leftTriangle(row-1, 0);
+            System.out.println();
         }
     }
 }
