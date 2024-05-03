21dc040d3ce62a56f76ecb4b856e3e4c9fc1d544
AnoopVL <avlanjekar4@gmail.com>
2024-05-03 22:14:17 +0530

    RecursionFactorial

--- printNos/src/printNos.java
+++ printNos/src/printNos.java
@@ -2,8 +2,9 @@
 public class printNos {
 
     public static void main(String[] args) {
-        printReverseNos(5);
-        printNos(5);
+        // printReverseNos(5);
+        // printNos(5);
+        factorial(5);
     }
     static void printReverseNos(int n){
         if (n<0) {
@@ -18,5 +19,13 @@ public class printNos {
         }
         printNos(n-1);
         System.out.println(n);
+    }
+    static int factorial(int n){
+        if (n==0) {
+            return 1;
+        }
+        int fact = n * factorial(n-1);
+        System.out.println(fact);
+        return fact;
     }
 }
