c9d911b46da5afe6d67882cf9c6db38edede2908
AnoopVL <avlanjekar4@gmail.com>
2024-05-12 12:36:10 +0530

    findAllIndexes Recursion

--- RecursionArray/src/RecursionArray.java
+++ RecursionArray/src/RecursionArray.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * RecursionArray
  */
@@ -6,7 +8,9 @@ public class RecursionArray {
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7};
         // System.out.println(checkArrSort(arr, 0));
-        System.out.println(searchArr(arr, 9, 0));
+        // System.out.println(searchArr(arr, 9, 0));
+        findAllIndexes(arr, 8, 0);
+        System.out.println(list);
     }
     public static boolean checkArrSort(int[] arr, int index){
         if(index == arr.length-1){
@@ -19,5 +23,15 @@ public class RecursionArray {
             return false;
         }
         return arr[index] == target || searchArr(arr, target, index+1);
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void findAllIndexes(int[]arr, int target, int index){
+        if(index == arr.length-1){
+            return;
+        } 
+        if(arr[index]==target){
+            list.add(index);
+        }
+        findAllIndexes(arr, target, index+1);
     }
 }
