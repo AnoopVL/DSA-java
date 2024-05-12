d9b3b08f3a03c205951c2ea467c4200bfa8bd361
AnoopVL <avlanjekar4@gmail.com>
2024-05-12 12:14:38 +0530

    array linear search recursion

--- RecursionArray/src/RecursionArray.java
+++ RecursionArray/src/RecursionArray.java
@@ -5,12 +5,19 @@ public class RecursionArray {
 
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7};
-        System.out.println(checkArrSort(arr, 0));
+        // System.out.println(checkArrSort(arr, 0));
+        System.out.println(searchArr(arr, 9, 0));
     }
     public static boolean checkArrSort(int[] arr, int index){
         if(index == arr.length-1){
             return true;
         }
         return arr[index] < arr[index+1] && checkArrSort(arr, index+1);
+    }
+    public static boolean searchArr(int[]arr, int target, int index){
+        if(index == arr.length-1){
+            return false;
+        }
+        return arr[index] == target || searchArr(arr, target, index+1);
     }
 }
