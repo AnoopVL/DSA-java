fc57cffd8785be7bcf7ce14d3f443998f5da17c6
AnoopVL <avlanjekar4@gmail.com>
2024-05-31 23:50:57 +0530

    QuickSort

---
+++ QuickSort/src/QuickSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        QuickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void QuickSort(int[]arr, int low, int high){
+        if(low>=high){
+            return;
+        }
+        int s = low;
+        int e = high;
+        int m = s + (e-s)/2;
+        int pivot = arr[m];
+        
+        while (s <= e) {
+            while (arr[s]< pivot) {
+                s++;
+            }
+            while (arr[e]>pivot) {
+                e--;
+            }
+            if (s<=e) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        QuickSort(arr, low, e);
+        QuickSort(arr, s, high);
+    }
+    
+}
