e4ce65f4c05e5d7a227aae6b0a7e083fe97c1970
AnoopVL <avlanjekar4@gmail.com>
2024-06-05 11:08:34 +0530

    subset with duplicates

--- Subset/src/Subset.java
+++ Subset/src/Subset.java
@@ -7,8 +7,11 @@ import java.util.List;
  */
 public class Subset {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        System.out.println(subsetIteration(arr));
+        // int[] arr = {1,2,3};
+        // System.out.println(subsetIteration(arr));
+        int[] arr = {1,2,2};
+        System.out.println(subsetIterationDuplicated(arr));
+
     }
     public static List<List<Integer>> subsetIteration(int[]arr){
         List<List<Integer>> outer = new ArrayList<>();
@@ -25,4 +28,28 @@ public class Subset {
         }
         return outer;
     } 
+
+    public static List<List<Integer>> subsetIterationDuplicated(int[]arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0; 
+        int end = 0;
+
+        for(int i = 0; i< arr.length-1; i++){
+            start =0;
+            if ( i > 0 && arr[i] == arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size() -1;
+            int n = outer.size();
+            for(int j = start; j < n; j++){
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(arr[i]);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    } 
+
 }
