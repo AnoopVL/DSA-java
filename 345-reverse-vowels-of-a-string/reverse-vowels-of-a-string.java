5c9038b2e8fc394df13a41be2f0d6f1dd1d2badf
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-06-14 12:02:11 +0530

    Time: 3 ms (80.41%) | Memory: 44.9 MB (36.12%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,46 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
-        StringBuilder str = new StringBuilder(s);
-        StringBuilder vowel = new StringBuilder();
+        char[] word = s.toCharArray();
+        int start =0 ;
+        int end  = s.length()-1;
+        String vowels = "AEIOUaeiou";
 
-        for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-            if(isVowel(ch)){
-                vowel.append(ch);
-                // str.charAt(i) = "_";
-                str.setCharAt(i,'_');
+        while(start < end){
+            // Move the pointers till we get a vowel
+            while(start < end && vowels.indexOf(word[start]) == -1){
+                start++;
             }
-        }
-        StringBuilder revVowel = new StringBuilder(vowel).reverse();
-
-        int vowelIndex = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '_') {
-                str.setCharAt(i, revVowel.charAt(vowelIndex));
-                vowelIndex++;
+            while(start < end && vowels.indexOf(word[end]) == -1){
+                end--;
             }
+            // Swap
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;   
+            // Move the pointers
+            start++;
+            end--;
         }
-
-        // for(int i=0; i < revVowel.length(); i++){
-        //     for(int j = 0; j < str.length(); j++){
-        //         // char ch = str.charAt(j);
-        //         if(str.charAt(j)=='_'){
-        //             str.charAt(j) = revVowel.charAt(i);
-        //             break;
-        //         }
-        //     }
-        // }
-        return str.toString();
-    }
-
-    public boolean isVowel(char ch){
-        ch = Character.toLowerCase(ch); // Ensure case-insensitive comparison
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-        // if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-        //     return true;
-        // }
-        // else{
-        //     return false;
-        // }
+        String answer = new String(word);
+        return answer;
     }
 }
