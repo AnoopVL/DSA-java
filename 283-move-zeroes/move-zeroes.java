f07fcea3c3d475da3e1c2b26da72d1b6bdac5b63
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-06-18 19:36:42 +0530

    Time: 2 ms (83.39%) | Memory: 45.9 MB (66.03%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,26 @@
 class Solution {
+    // run time 1ms
+    
     public void moveZeroes(int[] nums) {
-            int snowball = 0;
-            for(int i = 0; i < nums.length; i++){
-                if(nums[i]==0){
-                    snowball++;
-                }
-                else if(snowball > 0){
-                    nums[i-snowball] += nums[i];
-                    nums[i]=0;
-                }
-            }
+        int pos = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0)
+                nums[pos++] = nums[i];
+            
+        }
+        for(int i = pos; i < nums.length; i++)
+            nums[i] = 0;
             
     }
 }
+// run time 3ms
+ // int snowball = 0;
+            // for(int i = 0; i < nums.length; i++){
+            //     if(nums[i]==0){
+            //         snowball++;
+            //     }
+            //     else if(snowball > 0){
+            //         nums[i-snowball] += nums[i];
+            //         nums[i]=0;
+            //     }
+            // } 
