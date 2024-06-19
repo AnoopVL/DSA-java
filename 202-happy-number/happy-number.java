d3b2317ef96d7ba5af7338d964dd0967bdf27b17
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-06-19 12:03:42 +0530

    Time: 1 ms (67.84%) | Memory: 40.6 MB (54.75%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int fast = n;
+        int slow = n;
+        do{ 
+            slow = findSquare(slow);//we move this pointer by 1 step
+            fast = findSquare(findSquare(fast));//we move this pointer by 2 steps
+        }while (slow!=fast); 
+        if(slow ==1){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    private int findSquare(int num){
+        int ans = 0;
+        while(num >0){
+            int rem = num %10;
+            ans += rem*rem;
+            num/=10;
+        }
+        return ans;
+    }
+}
