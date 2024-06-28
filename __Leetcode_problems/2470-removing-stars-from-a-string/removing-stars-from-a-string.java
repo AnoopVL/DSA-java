48fbefc7aa2a8ac5f818dd9cd7af4d6b8351f585
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-06-28 19:14:46 +0530

    Time: 5 ms (100.00%) | Memory: 45.3 MB (85.00%) - LeetSync

--- __Leetcode_problems/2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ __Leetcode_problems/2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,15 +1,29 @@
 class Solution {
     public String removeStars(String s) {
-        StringBuilder ans = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i)!='*'){
-                ans.append(s.charAt(i));
-            }
-            else{
-                ans.deleteCharAt(ans.length() -1);
-            }
+       int len = s.length();
+        byte[] w = new byte[len];
+        s.getBytes(0, len, w, 0);
+
+        int j=0;
+        for (byte b : w) {
+            if (b == 42)
+                j--;
+            else
+                w[j++] = b;
         }
-        System.gc();
-        return ans.toString();
+        
+        return (new String(w, 0, j));
     }
 }
+
+//  StringBuilder ans = new StringBuilder();
+//         for(int i = 0; i < s.length(); i++){
+//             if(s.charAt(i)!='*'){
+//                 ans.append(s.charAt(i));
+//             }
+//             else{
+//                 ans.deleteCharAt(ans.length() -1);
+//             }
+//         }
+//         System.gc();
+//         return ans.toString();
