5ece7f3987ba6d6b997094c0da11717a67b5fe21
AnoopVL <avlanjekar4@gmail.com>
2024-07-06 17:24:29 +0530

    Binary Tree intro

--- BinaryTree/src/BinaryTree.java
+++ BinaryTree/src/BinaryTree.java
@@ -50,11 +50,34 @@ public class BinaryTree {
         display(root, "");
     }
     private void display(Node node, String indent){
-        if(node!=null){
+        if(node ==null){
             return;
         }
         System.out.println(indent + node.value);
         display(node.left, indent + "\t"); 
         display(node.right, indent + "\t"); 
     }
+
+    public void prettyDisplay(){
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) {
+          return;
+        }
+    
+        prettyDisplay(node.right, level + 1);
+    
+        if (level != 0) {
+          for (int i = 0; i < level - 1; i++) {
+            System.out.print("|\t\t");
+          }
+          System.out.println("|------->" + node.value);
+        } else {
+          System.out.println(node.value);
+        }
+        prettyDisplay(node.left, level + 1);
+      }
+    
 }
--- BinaryTree/src/main.java
+++ BinaryTree/src/main.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
-        tree.display();
+        // Scanner scanner = new Scanner(System.in);
+        // BinaryTree tree = new BinaryTree();
+        // tree.populate(scanner);
+        // // tree.display();
+        // tree.prettyDisplay();
+
+        BST bst = new BST();
+        int[] nums = {15,10,20,5,12,8};
+        bst.populate(nums);
+        bst.display();
     }
 }
