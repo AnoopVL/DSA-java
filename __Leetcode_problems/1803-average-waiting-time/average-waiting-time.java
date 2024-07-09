2b9d6859a4981b92bb0d50016d349dc25a198067
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-07-09 11:08:00 +0530

    Time: 3 ms (93.06%) | Memory: 72.8 MB (32.65%) - LeetSync

--- __Leetcode_problems/1803-average-waiting-time/average-waiting-time.java
+++ __Leetcode_problems/1803-average-waiting-time/average-waiting-time.java
@@ -1,29 +1,39 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int runningTime = customers[0][0];
-        int waitingTime = 0;
-        for(int i = 0; i< customers.length; i++){
-            if(runningTime >= customers[i][0]){
-                runningTime+=customers[i][1]; 
-                waitingTime = (runningTime) - customers[i][0];
-                list.add(waitingTime);
-                // runningTime+=customers[i][0]+customers[i][1];
-            }
-            else{
-                runningTime = customers[i][0] + customers[i][1];
-                waitingTime = runningTime - customers[i][0];
-                list.add(waitingTime);
-            }
+        double wait = 0;
+        int runTime = -1;
+        for(int[] a : customers) {
+            runTime = (runTime<a[0])?a[0]:runTime;
+            runTime += a[1];
+            wait += runTime-a[0];
         }
-        return calculateAverage(list);
-    }
-    public double calculateAverage(ArrayList<Integer> list) {
-        double sum = 0;
-        for (int number : list) {
-            sum += number;
-        }
-        
-        return sum / list.size();
+        return wait/customers.length;
     }
 }
+
+//     ArrayList<Integer> list = new ArrayList<>();
+    //     int runningTime = customers[0][0];
+    //     int waitingTime = 0;
+    //     for(int i = 0; i< customers.length; i++){
+    //         if(runningTime >= customers[i][0]){
+    //             runningTime+=customers[i][1]; 
+    //             waitingTime = (runningTime) - customers[i][0];
+    //             list.add(waitingTime);
+    //             // runningTime+=customers[i][0]+customers[i][1];
+    //         }
+    //         else{
+    //             runningTime = customers[i][0] + customers[i][1];
+    //             waitingTime = runningTime - customers[i][0];
+    //             list.add(waitingTime);
+    //         }
+    //     }
+    //     return calculateAverage(list);
+    // }
+    // public double calculateAverage(ArrayList<Integer> list) {
+    //     double sum = 0;
+    //     for (int number : list) {
+    //         sum += number;
+    //     }
+        
+    //     return sum / list.size();
+    // }
