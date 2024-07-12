857e43b9d99c8d2f22a895bb38f33093c5f80ef9
AnoopVL <avlanjekar4@gmail.com>
2024-07-13 00:22:19 +0530

    Segment tree code

--- BinaryTree/src/SegmentTree.java
+++ BinaryTree/src/SegmentTree.java
@@ -33,4 +33,67 @@ public class SegmentTree {
         node.data = node.left.data + node.right.data;
         return node;
     }
+    public void display(){
+        display(this.root);
+    }
+    private void display(Node node){
+        String str = "";
+        // display the left child
+        if (node.left != null) {
+            str = str + "Interval = [ " + node.left.startInterval+" - "+ node.left.endInterval + " ] data = " + node.left.data + " +=>";
+        }else{
+            str = str + "No left child";
+        }
+        // display the node
+        str = str + "Interval = [ " + node.startInterval+" - "+ node.endInterval + " ] data = " + node.data + " +=>";
+        // display right child
+        if (node.right != null) {
+            str = str + "Interval = [ " + node.right.startInterval+" - "+ node.right.endInterval + " ] data = " + node.right.data + " +=>";
+        }else{
+            str = str + "No right child";
+        }
+
+        System.out.println(str);
+
+        if (node.left != null) {
+            display(node.left);
+        }
+        if (node.right != null) {
+            display(node.right);
+        }
+    }
+
+    public int query(int qsi, int qei){
+        return this.query(this.root, qsi, qei);
+    }
+    private int query(Node node, int qsi, int qei) {
+        if(node.startInterval >= qsi && node.endInterval <= qei) {
+          // node is completely lying inside query
+          return node.data;
+        } else if (node.startInterval > qei || node.endInterval < qsi) {
+          // completely outside
+          return 0;
+        } else {
+          return this.query(node.left, qsi, qei) + this.query(node.right, qsi, qei);
+        }
+    }
+
+    // update
+    public void update(int index, int value) {
+        this.root.data = update(this.root, index, value);
+    }
+    private int update(Node node, int index, int value) {
+        if (index >= node.startInterval&& index <= node.endInterval){
+            if(index == node.startInterval && index == node.endInterval) {
+                node.data = value;
+                return node.data;
+            } else {
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+                node.data = leftAns + rightAns;
+                return node.data;
+            }
+        }
+        return node.data;
+    }
 }
--- BinaryTree/src/SegmentTreeMain.java
+++ BinaryTree/src/SegmentTreeMain.java
@@ -2,5 +2,6 @@ public class SegmentTreeMain {
     public static void main(String[] args) {
         int[] arr = {3,8,7,6,-2,-8,4,9};
         SegmentTree tree = new SegmentTree(arr);
+        System.out.println(tree.query(0, 7));
     }
 }
