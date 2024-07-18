2ff44169d567f5427265e91b240b0457dc6dd4e2
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-07-18 10:52:46 +0530

    Time: 2 ms (70.20%) | Memory: 61.8 MB (20.05%) - LeetSync

---
+++ __Leetcode_problems/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCurrent= 0, maxSoFar= 0;
+        for(int i=1; i< prices.length; i++){
+            maxCurrent=Math.max(0, maxCurrent+=prices[i]-prices[i-1]);
+            maxSoFar= Math.max(maxCurrent, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
