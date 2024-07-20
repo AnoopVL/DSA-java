edd5c18e39538f501ff28fdd728e21cd4a2b4312
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-07-20 23:10:17 +0530

    Time: 3 ms (42.81%) | Memory: 56.9 MB (72.13%) - LeetSync

---
+++ __Leetcode_problems/134-gas-station/gas-station.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalTank = 0;
+        int currentTank = 0;
+        int startingStation = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalTank += gas[i] - cost[i];
+            currentTank += gas[i] - cost[i];
+            // If current tank is negative, we cannot start from the current starting station
+            if (currentTank < 0) {
+                startingStation = i + 1; // Set next station as the starting one
+                currentTank = 0; // Reset current tank
+            }
+        }
+        // If total gas is greater than or equal to total cost, return the starting station index
+        return totalTank >= 0 ? startingStation : -1;
+    }
+}
