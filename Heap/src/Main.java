014a9f289db16332d33ed4a9d173d918574e2866
AnoopVL <avlanjekar4@gmail.com>
2024-08-02 23:25:42 +0530

    Heap

---
+++ Heap/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Heap<Integer> heap = new Heap<>();
+
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+
+        ArrayList list = heap.heapSort();
+        System.out.println(list);
+    }
+}
