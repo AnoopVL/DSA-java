d0f533cc8e3197509afb559f42658633f251cfc7
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-08-09 00:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 51.1 MB (42.56%) - LeetSync

--- __Leetcode_problems/975-range-sum-of-bst/range-sum-of-bst.java
+++ __Leetcode_problems/975-range-sum-of-bst/range-sum-of-bst.java
@@ -14,26 +14,22 @@
  * }
  */
 class Solution {
+    int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null){
-            return 0;
+            return sum;
         }
-        int ans=0;
-        // List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            TreeNode currentNode = queue.poll();
-            if(currentNode.val>= low && currentNode.val <= high){
-                ans+=currentNode.val;
-            }
-            if(currentNode.left!=null){
-                queue.offer(currentNode.left);
-            }
-            if(currentNode.right!=null){
-                queue.offer(currentNode.right);
-            }
+        if(root.val>=low&&root.val<=high){
+            rangeSumBST(root.left,low,high);
+            sum=sum+root.val;
+            rangeSumBST(root.right,low,high);
         }
-        return ans;
+        else if(root.val>high){
+            rangeSumBST(root.left,low,high);
+        }
+        else{
+            rangeSumBST(root.right,low,high);
+        }
+        return sum;
     }
 }
