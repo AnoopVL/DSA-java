2f5fa94691ae25ef184c719d33822cd6fe0c6ce2
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-08-09 16:40:23 +0530

    Time: 16 ms (23.01%) | Memory: 44.7 MB (16.96%) - LeetSync

---
+++ __Leetcode_problems/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0; i<t.length(); i++){
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(int v: map.values()){
+            if(v != 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
