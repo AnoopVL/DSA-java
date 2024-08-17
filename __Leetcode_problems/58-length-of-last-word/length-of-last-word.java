0030967bd7cd244decc9e4156ac39580f0cab490
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-08-17 14:32:43 +0530

    Time: 6 ms (5.03%) | Memory: 42 MB (6.00%) - LeetSync

--- __Leetcode_problems/58-length-of-last-word/length-of-last-word.java
+++ __Leetcode_problems/58-length-of-last-word/length-of-last-word.java
@@ -1,5 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        s= s.replaceAll("[^a-zA-Z ]", "");
         String st = s.trim();
         int length= 0;
         for(int i=st.length() -1 ; i>=0; i--){
