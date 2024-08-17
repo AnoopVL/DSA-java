40ef58be7b8382ea9e09bd559516e0ec24396062
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-08-17 20:03:57 +0530

    Time: 3 ms (99.70%) | Memory: 68.6 MB (34.65%) - LeetSync

--- __Leetcode_problems/234-palindrome-linked-list/palindrome-linked-list.java
+++ __Leetcode_problems/234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,26 +10,35 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head, prev, temp;
-        while (fast != null && fast.next != null) {
+        if(head == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null ){
             slow = slow.next;
             fast = fast.next.next;
         }
-        prev = slow;
+
+        ListNode temp;
+        ListNode prev = slow;
         slow = slow.next;
         prev.next = null;
-        while (slow != null) {
+        while(slow != null){
             temp = slow.next;
             slow.next = prev;
             prev = slow;
             slow = temp;
         }
+
         fast = head;
         slow = prev;
-        while (slow != null) {
-            if (fast.val != slow.val) return false;
-            fast = fast.next;
+        while(slow != null){
+            if(slow.val != fast.val){
+                return false;
+            }
             slow = slow.next;
+            fast = fast.next;
         }
         return true;
     }
