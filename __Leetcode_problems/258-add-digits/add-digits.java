0e4ee48f1aa0d826c33ac0de078d47c6f893f36e
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-08-28 12:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (41.36%) - LeetSync

---
+++ __Leetcode_problems/258-add-digits/add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num < 10){
+            return num;
+        }
+        int ans =0;
+        while(num > 0){
+            ans +=num%10;
+            num/=10;
+        }
+        return addDigits(ans);
+    }
+}
