6124c934357b2cfc8639fcc3bdc262586f037b2d
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-09-01 10:47:01 +0530

    Time: 3 ms (97.73%) | Memory: 55.6 MB (36.41%) - LeetSync

--- __Leetcode_problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ __Leetcode_problems/2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -1,22 +1,31 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        int [][] ans = new int[m][n];
         if(m*n != original.length){
-            // int[][] mt = new int[0][0];
             return new int[0][0];
         }
-        int row = 0;
-        int orgPtr= 0;
-        while (row < m && orgPtr < original.length){
-            int col = 0;
-            while(col < n){
-                ans[row][col] = original[orgPtr];
-                col++;
-                orgPtr++;
+        int [][] ans = new int[m][n];
+        int k=0;
+
+        for(int i =0; i< m; i++){
+            for(int j=0; j< n; j++){
+                ans[i][j] = original[k];
+                k++;
             }
-            row++;
         }
-        System.gc();
+
         return ans;
     }
 }
+
+// int row = 0;
+        // int orgPtr= 0;
+        // while (row < m && orgPtr < original.length){
+        //     int col = 0;
+        //     while(col < n){
+        //         ans[row][col] = original[orgPtr];
+        //         col++;
+        //         orgPtr++;
+        //     }
+        //     row++;
+        // }
+        // System.gc();
