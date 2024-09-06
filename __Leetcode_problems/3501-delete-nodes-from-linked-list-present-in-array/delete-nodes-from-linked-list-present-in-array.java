e9754dfa02b47743211eee35af69134bac0d1587
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-09-06 14:04:38 +0530

    Time: 20 ms (87.14%) | Memory: 63.5 MB (98.51%) - LeetSync

---
+++ __Leetcode_problems/3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// This one was already solved
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode current = head;
+        
+        while (current != null) {
+            if (numSet.contains(current.val)) {
+                prev.next = current.next;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+        
+        return dummy.next;
+    }
+}
