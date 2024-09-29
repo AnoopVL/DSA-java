663f37b75c6d125860b4acac616824ef6d7f1b2f
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-09-29 23:16:57 +0530

    Time: 13 ms (40.46%) | Memory: 45 MB (26.02%) - LeetSync

--- __Leetcode_problems/125-valid-palindrome/valid-palindrome.java
+++ __Leetcode_problems/125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        int start =0, end = s.length()-1;
-        while(start< end){
-            if(s.charAt(start) != s.charAt(end)){
+        int i= 0;
+        int j= s.length() -1;
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
-            start++;
-            end--;
+            i++;
+            j--;
         }
         return true;
     }
