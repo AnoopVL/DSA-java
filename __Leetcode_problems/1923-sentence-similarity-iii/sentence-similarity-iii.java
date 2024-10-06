7e5938503ee54dc8744238a7276bca6ea85bba16
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-10-06 17:30:53 +0530

    Time: 1 ms (96.43%) | Memory: 41.7 MB (39.29%) - LeetSync

---
+++ __Leetcode_problems/1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        if(sentence1.length()>sentence2.length()){
+            return areSentencesSimilar(sentence2, sentence1);
+        }
+        String smallerWords[] = sentence1.split(" ");
+        String largerWords[] = sentence2.split(" ");
+        int start=0;
+        int end1=smallerWords.length-1;
+        int end2=largerWords.length-1;
+        while(start<=end1 && smallerWords[start].equals(largerWords[start])){
+            start++;
+        }
+        while(start<=end1 && smallerWords[end1].equals(largerWords[end2])){
+            end1--;
+            end2--;
+        }
+        return (start>end1);
+    }
+}
