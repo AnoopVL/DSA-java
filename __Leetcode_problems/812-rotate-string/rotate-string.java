f5c8399f2f433aba46bde24ac48faed640647f26
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-11-09 13:10:21 +0530

    Time: 1 ms (19.49%) | Memory: 42.1 MB (6.91%) - LeetSync

--- __Leetcode_problems/812-rotate-string/rotate-string.java
+++ __Leetcode_problems/812-rotate-string/rotate-string.java
@@ -1,5 +1,12 @@
 class Solution {
-    public boolean rotateString(String A, String B) {
-        return A.length() == B.length() && (A + A).contains(B);
+    public boolean rotateString(String s, String goal) {
+        int n= s.length();
+        for(int i=0; i< s.length(); i++){
+            String rotateS = s.substring(i) + s.substring(0,i);
+            if(rotateS.equals(goal)){
+                return true;
+            } 
+        }
+        return false;
     }
 }
