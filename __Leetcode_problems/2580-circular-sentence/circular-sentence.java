5c84eee8192c6b30bdd6c08fea735864694696da
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-11-09 12:06:49 +0530

    Time: 1 ms (88.47%) | Memory: 41.9 MB (51.25%) - LeetSync

--- __Leetcode_problems/2580-circular-sentence/circular-sentence.java
+++ __Leetcode_problems/2580-circular-sentence/circular-sentence.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        if (sentence.charAt(0) != sentence.charAt(sentence.length() - 1))
-            return false;
-
-        int k = sentence.indexOf(" ");
-
-        if (k == -1)
-            return true;
-
-        while (k != -1) {
-            if (sentence.charAt(k - 1) != sentence.charAt(k + 1)) {
+        // sentence = sentence.toLowerCase();
+        String[] sentArray = sentence.split(" ");
+        if(sentArray.length == 1){
+            if(sentArray[0].charAt(0) != sentArray[0].charAt(sentArray[0].length() -1)){
                 return false;
             }
-
-            k = sentence.indexOf(" ", k+1);
-        }        
+            return true;
+        }
+        if(sentArray[0].charAt(0) != sentArray[sentArray.length -1].charAt((sentArray[sentArray.length -1]).length() -1)){
+            return false;
+        }
+        
+        for(int i=1; i < sentArray.length; i++){
+            if(sentArray[i].charAt(0) != sentArray[i-1].charAt(sentArray[i-1].length() -1)){
+                return false;
+            }
+            if(i != sentArray.length -1){
+                if(sentArray[i].charAt(sentArray[i].length() -1) != sentArray[i+1].charAt(0)){
+                    return false;
+                }
+            }
+        }
         return true;
+
     }
 }
