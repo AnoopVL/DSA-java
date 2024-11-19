bdc78ffc5f9adf87d5b4a4e71792a9b3b85ac8b3
Anoop Lanjekar <avlanjekar4@gmail.com>
2024-11-19 20:46:02 +0530

    Time: 2 ms (67.35%) | Memory: 42.2 MB (55.54%) - LeetSync

--- __Leetcode_problems/1755-defuse-the-bomb/defuse-the-bomb.java
+++ __Leetcode_problems/1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int[] res = new int[code.length];
-        if (k == 0) return res;
-        int start = 1, end = k, sum = 0;
-        if (k < 0) {
-            k = -k;
-            start = code.length - k;
-            end = code.length - 1;
+        int n = code.length;
+        int[] ans = new int[n];
+        if(k==0){
+            return ans;
         }
-        for (int i = start; i <= end; i++) sum += code[i];
-        for (int i = 0; i < code.length; i++) {
-            res[i] = sum;
-            sum -= code[(start++) % code.length];
-            sum += code[(++end) % code.length];
+        if(k > 0){
+            for(int i=0; i< n; i++){
+                int sum=0;
+                for(int j=1; j <= k; j++){
+                    int circularIndex = (i+j) % n; 
+                    sum+=code[circularIndex];
+                }
+                ans[i]=sum;
+            }
         }
-        return res;
+        if(k < 0){
+            for(int i=0; i< n; i++){
+                int sum = 0;
+                for(int j= 1; j <= Math.abs(k); j++){
+                    int circularIndex = (i-j+n) %n;
+                    sum+=code[circularIndex];
+                }
+                ans[i] = sum;
+            }
+        }
+        return ans;
     }
 }
