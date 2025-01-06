18b75fc2432f904c14ca2009110824500f0f4e65
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-01-06 13:01:54 +0530

    Time: 65 ms (46.01%) | Memory: 44.6 MB (77.78%) - LeetSync

---
+++ __Leetcode_problems/1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        List<Integer> ballLoc = new ArrayList<>();
+        for(int i=0; i< n; i++){
+            if(boxes.charAt(i) == '1') ballLoc.add(i);
+        }
+        int[] ans = new int[n];
+        int ansIndex=0;
+        for(int i=0; i< n; i++){
+            int minMov = 0;
+            for(int j=0; j< ballLoc.size(); j++){
+                minMov += Math.abs(i - ballLoc.get(j));
+            }
+            ans[ansIndex] = minMov;
+            ansIndex++;  
+        }
+        return ans;
+    }
+}
