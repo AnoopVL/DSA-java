98d26266f3ac98d344747716603128c8d09bbe1f
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-01-27 00:31:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,63 +1,61 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class gfg {
-    public static void main(String[] args) throws Exception {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            int w = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            // Read capacity
+            int capacity = Integer.parseInt(br.readLine());
 
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            List<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
+            // Read values
+            String[] valInput = br.readLine().split(" ");
+            int[] val = new int[valInput.length];
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
             }
 
-            int[] val = new int[array.size()];
-            int idx = 0;
-            for (int i : array) val[idx++] = i;
-
-            String lin = br.readLine();
-            String[] toke = lin.split(" ");
-            List<Integer> array1 = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : toke) {
-                array1.add(Integer.parseInt(token));
+            // Read weights
+            String[] wtInput = br.readLine().split(" ");
+            int[] wt = new int[wtInput.length];
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            int[] wt = new int[array1.size()];
-            idx = 0;
-            for (int i : array1) wt[idx++] = i;
-
-            // calling method knapSack() of class Solution
-            System.out.println(new Solution().knapSack(w, wt, val));
+            // Call the knapSack function and print the result
+            System.out.println(Solution.knapSack(capacity, val, wt));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // This is recursive approach
-    static int knapSack(int W, int wt[], int val[]) {
-       return helper(wt, val, W, val.length);
-    }
-    
-    private static int helper(int[] wt, int[] val, int W, int  n){
-        if( n==0 || W ==0){
-            return 0;
+    // Function to return max value that can be put in knapsack of capacity.
+    static int knapSack(int W, int val[], int wt[]) {
+        // code here
+        int n= wt.length;
+        //Initialize the matrix
+        int t[][] = new int[n+1][W+1];
+        
+        for(int i=0; i< n+1; i++){
+            for(int j=0; j< W+1; j++){
+                // Base condition(where we return 0 in the recursion code)
+                if(i==0 || j==0){
+                    t[i][j] = 0;
+                }else if(wt[i-1] <= j){
+                    t[i][j] = Math.max(val[i-1] + t[i-1][j - wt[i-1]], t[i-1][j]);
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
-        if(wt[n-1] <= W){
-            return Math.max(val[n-1] + helper(wt, val, W-wt[n-1], n-1), helper(wt, val, W, n-1));
-        }else{
-            return helper(wt, val, W, n-1);
-        }
+        return t[n][W];
     }
 }
