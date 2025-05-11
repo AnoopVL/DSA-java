7781036096fd5d4eeb3976cfda08ae680f321e37
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-05-11 12:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (55.09%) - LeetSync

--- __Leetcode_problems/1293-three-consecutive-odds/three-consecutive-odds.java
+++ __Leetcode_problems/1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        if(arr.length <3){
-            return false;
+        if(arr.length < 3)return false;
+        for(int i=0; i< arr.length -2; i++){
+            if(arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0) return true;
         }
-        boolean consecutive = false;
-        for(int i = 0; i< arr.length-2; i++){
-            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
-                consecutive = true;
-                break;
-            }
-        }
-        return consecutive;
+        return false;
     }
 }
