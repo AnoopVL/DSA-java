7a4c38cb601acada7fc942627d1410be5c3c420a
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-07-20 11:44:28 +0530

    Time: 8 ms (16.24%) | Memory: 46.2 MB (41.96%) - LeetSync

---
+++ __Leetcode_problems/210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int n, int[][] preR) {
+        // Build adjacency list
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        int m = preR.length;
+        for(int i = 0; i < m; i++){
+            adj.get(preR[i][1]).add(preR[i][0]);
+        }
+        // create an array indegree
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int j : adj.get(i)){
+                indegree[j]++;
+            }
+        }
+        // create a queue and add all the elements with indegree 0
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+        List<Integer> ansList = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ansList.add(node);
+            for(int j : adj.get(node)){
+                indegree[j]--;
+                if(indegree[j] == 0) q.add(j);
+            }
+        }
+        if(ansList.size() != n){
+            return new int[0];
+        }else{
+            return ansList.stream().mapToInt(i -> i).toArray();
+        }
+    }
+}
