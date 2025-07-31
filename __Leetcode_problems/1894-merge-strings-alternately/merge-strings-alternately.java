697e37fb20bb0d553ef5e183e059c9089b91ec6e
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-07-31 11:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (56.51%) - LeetSync

--- __Leetcode_problems/1894-merge-strings-alternately/merge-strings-alternately.java
+++ __Leetcode_problems/1894-merge-strings-alternately/merge-strings-alternately.java
@@ -3,15 +3,13 @@ class Solution {
         int n = Math.min(word1.length(), word2.length());
         StringBuilder finalString = new StringBuilder();
         for(int i = 0; i < n; i++){
-            // This is wrong
-        //    finalString.append(word1.charAt(i)+word2.charAt(i));
             finalString.append(word1.charAt(i));
             finalString.append(word2.charAt(i));
         }
         if (word1.length() > n) {
-            finalString.append(word1.substring(n));
+            finalString.append(word1.substring(n, word1.length()));
         } else if (word2.length() > n) {
-            finalString.append(word2.substring(n));
+            finalString.append(word2.substring(n, word2.length()));
         }
         return finalString.toString();
     }
