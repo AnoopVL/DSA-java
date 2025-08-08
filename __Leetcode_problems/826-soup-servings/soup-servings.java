6794319b95859a7dabfdcd56b3b7b36915b8a728
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-08-08 23:37:46 +0530

    Time: 86 ms (6.59%) | Memory: 284.8 MB (5.39%) - LeetSync

---
+++ __Leetcode_problems/826-soup-servings/soup-servings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] operations = {{100,0},{75,25},{50,50},{25,75}};
+   
+    public double soupServings(int n) {
+        if(n > 10000) return 1;
+        double[][] memo = new double[n+1][n+1];
+        for(double[] mem : memo){
+            Arrays.fill(mem, -1.0);
+        }
+        return solve(n,n, memo);
+    }
+
+    private double solve(int a, int b, double[][] memo){
+        if(a <= 0 && b <= 0) return 0.5;
+        if(a <= 0) return 1.0;
+        if(b <= 0) return 0.0;
+        if(memo[a][b] != -1.0) return memo[a][b];
+        double probablity = 0.0;
+        for(int[] opp : operations){
+            int aTaken = opp[0];
+            int bTaken = opp[1];
+            probablity += solve(a - aTaken, b - bTaken, memo);
+        }
+        return memo[a][b] = 0.25 * probablity;
+    }
+}
