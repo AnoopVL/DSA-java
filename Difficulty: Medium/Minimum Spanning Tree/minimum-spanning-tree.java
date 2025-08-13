3ef12c38eda9844df7d8b6605674c7c3babf4208
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-08-13 14:10:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,34 +1,47 @@
 class Solution {
+    static class DisjointSet{
+        int[] parent;
+        int[] size;
+        DisjointSet(int n){
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        int find(int x){
+           if (parent[x] != x) parent[x] = find(parent[x]);
+           return parent[x];
+        }
+        boolean union(int u, int v){
+            int ulpU = find(u);
+            int ulpV = find(v);
+            if(ulpU == ulpV) return false;
+            if(size[ulpU] < size[ulpV]){
+                parent[ulpU] = ulpV;
+                size[ulpV] += size[ulpU];
+            }else{
+                parent[ulpV] = ulpU;
+                size[ulpU] += size[ulpV];
+            }
+            return true;
+        }
+    }
     public int spanningTree(int V, int[][] edges) {
         // code here
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        for(int i = 0 ; i < edges.length ; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-            adj.get(u).add(new int[]{v, wt});
-            adj.get(v).add(new int[]{u, wt});
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> (x[1] - y[1]));
-        pq.add(new int[]{0,0});
-        int[] vis = new int[V];
-        int sum = 0;
-        
-        while(!pq.isEmpty()){
-            int[] poll = pq.poll();
-            int node = poll[0];
-            int wt = poll[1];
-            if(vis[node] == 1) continue;
-            vis[node] = 1;
-            sum += wt;
-            for (int[] edge : adj.get(node)) {
-                int adjNode = edge[0];
-                int edW = edge[1];
-                if (vis[adjNode] == 0) pq.add(new int[]{adjNode, edW});
+        Arrays.sort(edges,(x,y) -> x[2] - y[2]);
+        DisjointSet dsu = new DisjointSet(V);
+        int ansWeight = 0;
+        int edgesUsed = 0;
+        for(int[] edge: edges){
+            if(dsu.union(edge[0], edge[1])){
+                ansWeight += edge[2];
+                edgesUsed++;
+                if(edgesUsed == V-1) break;
             }
-
         }
-        return sum;
+        return ansWeight;
+
     }
 }
