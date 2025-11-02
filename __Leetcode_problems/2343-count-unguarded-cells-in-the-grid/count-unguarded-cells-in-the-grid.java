de77ede27afcef8165029a794fc44bce01505eb3
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-11-02 14:02:24 +0530

    Time: 22 ms (71.53%) | Memory: 147.1 MB (5.11%) - LeetSync

--- __Leetcode_problems/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ __Leetcode_problems/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        // int guardedCount = guards.length + walls.length;
-        int ans = 0;
+        int guardedCount = guards.length + walls.length;
+        // int ans = 0;
         int[][] grid = new int[m][n];
         for(int[] cell : guards){
             grid[cell[0]][cell[1]] = 2;
@@ -20,15 +20,14 @@ class Solution {
                 while(x+dx>=0 && x+dx<m && y+dy>=0 && y+dy<n && grid[x+dx][y+dy]<2){
                     x += dx;
                     y += dy;
-                    grid[x][y] = 1;
+                    if(grid[x][y] == 0){
+                        grid[x][y] = 1;
+                        guardedCount++;
+                    }
                 }
             }
         }
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 0) ans++;
-            }
-        }
-        return ans;
+        return (m*n) - guardedCount;
+        
     }
 }
