5dcdbeb4e02530890def2ee2959df10b3885c623
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-11-02 13:43:03 +0530

    Time: 22 ms (71.53%) | Memory: 147.4 MB (5.11%) - LeetSync

--- __Leetcode_problems/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ __Leetcode_problems/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,36 +1,34 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] g = new int[m][n];        
-        for (int[] e : guards) {
-            g[e[0]][e[1]] = 2;
+        // int guardedCount = guards.length + walls.length;
+        int ans = 0;
+        int[][] grid = new int[m][n];
+        for(int[] cell : guards){
+            grid[cell[0]][cell[1]] = 2;
         }
-        for (int[] e : walls) {
-            g[e[0]][e[1]] = 2;
+        for(int[] cell : walls){
+            grid[cell[0]][cell[1]] = 2;
         }
-        
-        int[] dirs = {-1, 0, 1, 0, -1};
-        
-        for (int[] e : guards) {
-            for (int k = 0; k < 4; ++k) {
-                int x = e[0], y = e[1];
-                int dx = dirs[k], dy = dirs[k + 1];
+        int[] dir = {-1, 0, 1, 0, -1};
+        for(int[] cell : guards){
+            for(int i = 0; i< 4; i++){
+                int x = cell[0];
+                int y = cell[1];
+                int dx = dir[i];
+                int dy = dir[i+1];
                 
-                while (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n && g[x + dx][y + dy] < 2) {
+                while(x+dx>=0 && x+dx<m && y+dy>=0 && y+dy<n && grid[x+dx][y+dy]<2){
                     x += dx;
                     y += dy;
-                    g[x][y] = 1;
-                }
-            }
-        }        
-        int unguardedCount = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (g[i][j] == 0) {
-                    unguardedCount++;
+                    grid[x][y] = 1;
                 }
             }
         }
-        
-        return unguardedCount;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0) ans++;
+            }
+        }
+        return ans;
     }
 }
