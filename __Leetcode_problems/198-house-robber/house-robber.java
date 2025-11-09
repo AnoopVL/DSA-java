0e9376a6016dc34134314789bd6f0df4fced30e1
Anoop Lanjekar <avlanjekar4@gmail.com>
2025-11-09 15:34:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (11.16%) - LeetSync

--- __Leetcode_problems/198-house-robber/house-robber.java
+++ __Leetcode_problems/198-house-robber/house-robber.java
@@ -1,20 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n-1, nums, dp);
-    }
+        int prev = nums[0];
+        int prev2 = 0;
 
-    private int helper(int index, int[] nums, int[] dp){
-        if(index == 0) return nums[index];
-        if(index < 0) return 0;
-        if(dp[index] != -1) return dp[index];
-
-        int rob = nums[index] + helper(index - 2, nums, dp);
-        int dont_rob = 0 + helper(index - 1, nums, dp);
-
-        return dp[index] = Math.max(rob, dont_rob);
+        for(int i = 1; i < n; i++){
+            int rob = nums[i];
+            if(i > 1) rob += prev2;
+            int dont_rob = 0 + prev;
+            int curr_i = Math.max(rob, dont_rob);
+            prev2 = prev;
+            prev = curr_i;
+        }
+        return prev;
     }
 
 }
